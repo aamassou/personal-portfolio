7ad043992a25514e0af2b86834d6b9e2682a9536
Azmin Massoumi <azmin.massoumi@gmail.com>
2020-06-22 17:19:03 +0800

    about section responsive, I think

--- js/main.js
+++ js/main.js
@@ -2,24 +2,26 @@ var sPos = 0;
 var subMenus = document.getElementsByClassName('sub-menu');
 var menuHeadings = document.getElementsByClassName('menu-heading');
 
+var subMenuShown = false;
 function showSubMenu(menu) {
     var about = document.getElementById("about-text");
-    about.style.transform = "translate(0, -130px)"
-
+    
     about.querySelector('h3').style.opacity = 0;
     about.querySelector('h5').style.opacity = 0;
-
+    
+    about.style.transform = "translate(0, -130px)"
+    document.getElementById("about-menus").style.transform = "translate(0, -130px)"
     for(let i=0; i<subMenus.length; i++) {
         subMenus[i].style.opacity = 0;
     }
-
+    
     var subMenu = document.getElementById(menu);
     subMenu.style.opacity = 1;
-
+    
     for(let i=0; i<menuHeadings.length;i++) {
         menuHeadings[i].classList.remove('menu-active')
     }
-
+    
     var heading = document.getElementById(menu+'-menu');
     heading.classList.add('menu-active');
 
@@ -27,11 +29,17 @@ function showSubMenu(menu) {
         document.getElementById('exit-menu').style.opacity = 1;
     },500);
     
+    if(mobileMenuShown) {
+        showHeaderMenu();
+    }
+    
+    subMenuShown = true;
 }
 
 function hideSubMenu() {
     var about = document.getElementById("about-text");
     about.style.transform = "translate(0, 0)"
+    document.getElementById("about-menus").style.transform = "translate(0, 0)"
     
     about.querySelector('h3').style.opacity = 1;
     about.querySelector('h5').style.opacity = 1;
@@ -45,7 +53,7 @@ function hideSubMenu() {
         }
 
     document.getElementById('exit-menu').style.opacity = 0;
-
+    subMenuShown = false;
     }
 
 function slideChange(val) {
@@ -87,4 +95,38 @@ function sendMsg() {
     nameInput.value = "";
     emailInput.value = "";
     msgInput.value = "";
+}
+
+var mobileMenuShown = false;
+function showHeaderMenu() {
+    if(subMenuShown) {
+        hideSubMenu();
+    }
+
+    var mobileMenu = document.getElementById("mobile-sub-menu");
+
+    if(mobileMenuShown) {
+        mobileMenu.style.opacity = 0;
+        mobileMenu.style.pointerEvents = "none";
+
+        document.getElementById("times").style.display = 'none';
+        document.getElementById("times").style.opacity = 0;
+
+        document.getElementById("bars").style.opacity = 1;
+        document.getElementById("bars").display = 'block';
+        
+        mobileMenuShown = false;
+    }
+    
+    else {
+        mobileMenu.style.opacity = 1;
+        mobileMenu.style.pointerEvents = "all";
+    
+        document.getElementById("times").style.display = 'block';
+        document.getElementById("times").style.opacity = 1;
+
+        document.getElementById("bars").display = 'none';
+        document.getElementById("bars").style.opacity = 0;
+        mobileMenuShown = true;
+    }
 }
