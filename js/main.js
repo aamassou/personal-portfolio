83cb3fccb44da5ece6ff0cf7e5b70b0a695d4dcf
Azmin Massoumi <azmin.massoumi@gmail.com>
2020-06-22 12:31:15 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,75 @@
+var sPos = 0;
+var subMenus = document.getElementsByClassName('sub-menu');
+var menuHeadings = document.getElementsByClassName('menu-heading');
+
+function showSubMenu(menu) {
+    var about = document.getElementById("about-text");
+    about.style.transform = "translate(0, -130px)"
+
+    about.querySelector('h3').style.opacity = 0;
+    about.querySelector('h5').style.opacity = 0;
+
+    for(let i=0; i<subMenus.length; i++) {
+        subMenus[i].style.opacity = 0;
+    }
+
+    var subMenu = document.getElementById(menu);
+    subMenu.style.opacity = 1;
+
+    for(let i=0; i<menuHeadings.length;i++) {
+        menuHeadings[i].classList.remove('menu-active')
+    }
+
+    var heading = document.getElementById(menu+'-menu');
+    heading.classList.add('menu-active');
+
+    setTimeout(2000);
+    document.getElementById('exit-menu').style.opacity = 1;
+}
+
+function hideSubMenu() {
+    var about = document.getElementById("about-text");
+    about.style.transform = "translate(0, 0)"
+
+    about.querySelector('h3').style.opacity = 1;
+    about.querySelector('h5').style.opacity = 1;
+    
+    for(let i=0; i<subMenus.length; i++) {
+        subMenus[i].style.opacity = 0;
+    }
+    
+    for(var i=0; i<menuHeadings.length;i++) {
+            menuHeadings[i].classList.remove('menu-active')
+        }
+}
+
+function slideChange(val) {
+    sPos += val;
+
+    if(sPos > 0) {
+        sPos = -200;
+    }
+
+    else if(sPos < -200) {
+        sPos = 0;
+    }
+
+    jumpSlide(sPos);
+}
+
+function jumpSlide(pos) {
+    sPos = Number(pos);
+    document.getElementById("slider").style.transform = `translate(${sPos}%, 0)`;
+    updateNav();
+}
+
+function updateNav() {
+    
+    var navBars = document.getElementsByClassName("quick-nav");
+    for(let j=0; j<navBars.length;j++) {
+        navBars[j].classList.remove('nav-active');
+    }
+
+    var i = (sPos/-100);
+    navBars[i].classList.add('nav-active');
+}
