e48b72ac92112d840ade087ab41c75fdfe2731a5
Azmin Massoumi <azmin.massoumi@gmail.com>
2020-06-24 14:51:19 +0800

    add animations to experience and projects

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-var sPos = 0;
-var subMenus = document.getElementsByClassName('sub-menu');
+var about = document.getElementById("about-text");
 var menuHeadings = document.getElementsByClassName('menu-heading');
+var aboutMenus = document.getElementById("about-menus");
+var subMenus = document.getElementsByClassName('sub-menu');
+
+var sPos = 0;
+var slider = document.getElementById("slider");
+var navBars = document.getElementsByClassName("quick-nav");
 
 var subMenuShown = false;
 
@@ -16,13 +21,12 @@ function toggleSubMenu(menu) {
 }
 
 function showSubMenu(menu) {
-    var about = document.getElementById("about-text");
     
     about.querySelector('h3').style.opacity = 0;
     about.querySelector('h5').style.opacity = 0;
     
     about.style.transform = "translate(0, -130px)"
-    document.getElementById("about-menus").style.transform = "translate(0, -130px)"
+    aboutMenus.style.transform = "translate(0, -130px)"
     for(let i=0; i<subMenus.length; i++) {
         subMenus[i].style.opacity = 0;
     }
@@ -36,10 +40,6 @@ function showSubMenu(menu) {
     
     var heading = document.getElementById(menu+'-menu');
     heading.classList.add('menu-active');
-
-    // setTimeout(function() {
-    //     document.getElementById('exit-menu').style.opacity = 1;
-    // },500);
     
     if(mobileMenuShown) {
         showHeaderMenu();
@@ -49,9 +49,8 @@ function showSubMenu(menu) {
 }
 
 function hideSubMenu() {
-    var about = document.getElementById("about-text");
     about.style.transform = "translate(0, 0)"
-    document.getElementById("about-menus").style.transform = "translate(0, 0)"
+    aboutMenus.style.transform = "translate(0, 0)"
     
     about.querySelector('h3').style.opacity = 1;
     about.querySelector('h5').style.opacity = 1;
@@ -64,7 +63,6 @@ function hideSubMenu() {
             menuHeadings[i].classList.remove('menu-active')
         }
 
-    // document.getElementById('exit-menu').style.opacity = 0;
     subMenuShown = false;
     }
 
@@ -84,13 +82,12 @@ function slideChange(val) {
 
 function jumpSlide(pos) {
     sPos = Number(pos);
-    document.getElementById("slider").style.transform = `translate(${sPos}%, 0)`;
+    slider.style.transform = `translate(${sPos}%, 0)`;
     updateNav();
 }
 
 function updateNav() {
     
-    var navBars = document.getElementsByClassName("quick-nav");
     for(let j=0; j<navBars.length;j++) {
         navBars[j].classList.remove('nav-active');
     }
@@ -108,33 +105,66 @@ setInterval(function() {
     jumpSlide(sPos);
 }, 3000)
 
-function sendMsg() {
-    var nameInput = document.getElementById("name");
-    var emailInput = document.getElementById("email");
-    var msgInput = document.getElementById("message");
+var expOne = document.getElementById("expOne");
+var expTwo = document.getElementById("expTwo");
+var expThree = document.getElementById("expThree");
 
-    nameInput.value = "";
-    emailInput.value = "";
-    msgInput.value = "";
-}
+var projOne = document.getElementById("projOne");
+var projTwo = document.getElementById("projTwo");
+var projThree = document.getElementById("projThree");
+window.addEventListener('scroll', function() {
+    if(window.scrollY >= (expOne.offsetTop) - 700) {
+        expOne.style.opacity = 1;
+        expOne.style.animation = 'fadeInExperience 2s';
+
+        setTimeout(function() {
+            expTwo.style.opacity = 1;
+            expTwo.style.animation = 'fadeInExperience 2s';
+            // expTwo.style.animationDelay = '1s';
+        }, 750)
+        
+        setTimeout(function() {
+            expThree.style.opacity = 1;
+            expThree.style.animation = 'fadeInExperience 2s';
+            // expThree.style.animationDelay = '2s';
+        },1500);
+        
+    }
+
+    if(window.scrollY >= (projOne.offsetTop) - 700) {
+        projOne.style.opacity = 1;
+        projOne.style.animation = 'fadeInProject 2s';
+    }
+
+    if(window.scrollY >= (projTwo.offsetTop) - 700) {
+        projTwo.style.opacity = 1;
+        projTwo.style.animation = 'fadeInProject 2s';
+    }
+
+    if(window.scrollY >= (projThree.offsetTop) - 700) {
+        projThree.style.opacity = 1;
+        projThree.style.animation = 'fadeInProject 2s';
+    }
+});
+
 
 var mobileMenuShown = false;
+var mobileMenu = document.getElementById("mobile-sub-menu");
+
 function showHeaderMenu() {
     if(subMenuShown) {
         hideSubMenu();
     }
 
-    var mobileMenu = document.getElementById("mobile-sub-menu");
-
     if(mobileMenuShown) {
         mobileMenu.style.opacity = 0;
         mobileMenu.style.pointerEvents = "none";
 
-        document.getElementById("times").style.display = 'none';
-        document.getElementById("times").style.opacity = 0;
+        document.getElementById("mobile-hide").style.display = 'none';
+        document.getElementById("mobile-hide").style.opacity = 0;
 
-        document.getElementById("bars").style.opacity = 1;
-        document.getElementById("bars").display = 'block';
+        document.getElementById("mobile-show").style.opacity = 1;
+        document.getElementById("mobile-show").display = 'block';
         
         mobileMenuShown = false;
     }
@@ -143,11 +173,11 @@ function showHeaderMenu() {
         mobileMenu.style.opacity = 1;
         mobileMenu.style.pointerEvents = "all";
     
-        document.getElementById("times").style.display = 'block';
-        document.getElementById("times").style.opacity = 1;
+        document.getElementById("mobile-hide").style.display = 'block';
+        document.getElementById("mobile-hide").style.opacity = 1;
 
-        document.getElementById("bars").display = 'none';
-        document.getElementById("bars").style.opacity = 0;
+        document.getElementById("mobile-show").display = 'none';
+        document.getElementById("mobile-show").style.opacity = 0;
         mobileMenuShown = true;
     }
 }
@@ -162,4 +192,14 @@ function hideModal() {
     document.getElementById("modal").style.display = "none";
     document.querySelector("body").style.overflow = "auto";
     document.querySelector("header").style.zIndex = 4;
+}
+
+function sendMsg() {
+    var nameInput = document.getElementById("name");
+    var emailInput = document.getElementById("email");
+    var msgInput = document.getElementById("message");
+
+    nameInput.value = "";
+    emailInput.value = "";
+    msgInput.value = "";
 }
